cda9ceafb530ec321f3c4332a48c520c1023699d
baohng <bao.hng221b@gmail.com>
2023-12-23 22:20:25 +0700

    add header

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,5 +1,32 @@
+import "./header.css";
+
 function Header() {
-  return <div>This is the heather</div>;
+  return (
+    <div className="header">
+      <div className="header-logo">
+        <i className="fi-rr-layout-fluid"></i>
+      </div>
+      <div className="header-branch">
+        <label className="header-label">Task-Trackr</label>
+      </div>
+      <div className="header-center">
+        <div className="header-search">
+          <i className="fi-rr-search"></i>
+          <input placeholder="Search..." />
+        </div>
+
+        <div className="header-profile">
+          <div className="header-options">
+            <i className="fi-rr-menu-dots"></i>
+          </div>
+          <img
+            className="header-avatar"
+            src="https://cdn-icons-png.flaticon.com/128/4333/4333609.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
